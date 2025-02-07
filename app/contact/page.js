3e8fe7f6dc914c06cd07f843d406c3276b27441c
Ryan Paulson <ryanpdesigns@gmail.com>
2025-02-07 14:58:23 -0600

    contact form working

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,13 +1,64 @@
 'use client';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
-	const handleClick = () => {
-		console.log('button clicked');
+	const [isChecked, setIsChecked] = useState(false);
+	const blankInfo = {
+		firstName: '',
+		lastName: '',
+		email: '',
+		phone: '',
+	};
+	const [info, setInfo] = useState(blankInfo);
+	const checkHandler = () => {
+		setIsChecked(!isChecked);
+	};
+	const changeHandler = (name) => {
+		return (e) => {
+			setInfo({ ...info, [name]: e.target.value });
+		};
+	};
+	const handleClick = (e) => {
+		e.preventDefault();
+		if (isChecked == true) {
+			setInfo(blankInfo);
+			setIsChecked(false);
+			sendEmail(info);
+		} else {
+			console.log('Checkbox not checked');
+		}
+	};
+	emailjs.init({
+		publicKey: '2FpEKU9Zq_60k7nSi',
+		// allow headless browsers
+		blockHeadless: false,
+		blockList: {
+			// Block the suspended emails
+			list: [],
+			// The variable contains the email address
+			watchVariable: 'email',
+		},
+	});
+	const sendEmail = (params) => {
+		try {
+			emailjs.send('default_service', 'generic_contact', params).then(
+				(response) => {
+					console.log('Email sent', response.status, response.text);
+				},
+				(err) => {
+					console.error('FAILED...', err);
+				}
+			);
+		} catch (error) {
+			console.error(error);
+		}
 	};
 	return (
 		<div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<h2 className="text-2xl text-center sm:text-left">
-					Contact Us for more information
+				<h2 className="text-3xl text-center sm:text-left">
+					Contact Us For More Information
 				</h2>
 				<div>
 					<form
@@ -16,24 +67,50 @@ export default function Contact() {
 						<div className="flex flex-col">
 							<label htmlFor="firstName">First Name</label>
 							<input
+								value={info.firstName}
+								onChange={changeHandler('firstName')}
 								type="text"
 								name="firstName"
-								required></input>
+								required
+								className="text-black px-1 w-[250px] md:w-[500px]"></input>
 						</div>
 						<div className="flex flex-col">
 							<label htmlFor="lastName">Last Name</label>
-							<input type="text" name="lastName" required></input>
+							<input
+								value={info.lastName}
+								onChange={changeHandler('lastName')}
+								type="text"
+								name="lastName"
+								required
+								className="text-black px-1 w-[250px] md:w-[500px]"></input>
 						</div>
 						<div className="flex flex-col">
 							<label htmlFor="emailName">Email</label>
-							<input type="email" name="email" required></input>
+							<input
+								value={info.email}
+								onChange={changeHandler('email')}
+								type="email"
+								name="email"
+								required
+								className="text-black px-1 w-[250px] md:w-[500px]"></input>
 						</div>
 						<div className="flex flex-col">
 							<label htmlFor="phone">Phone</label>
-							<input type="tel" name="phone"></input>
+							<input
+								value={info.phone}
+								onChange={changeHandler('phone')}
+								type="tel"
+								name="phone"
+								className="text-black px-1 w-[250px] md:w-[500px]"></input>
 						</div>
 						<div className="flex flex-row gap-2">
-							<input type="checkbox" name="checkbox" required />
+							<input
+								checked={isChecked}
+								onChange={checkHandler}
+								type="checkbox"
+								name="checkbox"
+								required
+							/>
 							<label htmlFor="checkbox">
 								I agree to be contacted
 							</label>
