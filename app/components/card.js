eeecee28a4aa86fed35d50d96014a1938363bc4d
Ryan Paulson <ryanpdesigns@gmail.com>
2025-02-07 13:06:47 -0600

    added description to home page cards

--- app/components/card.js
+++ app/components/card.js
@@ -15,10 +15,10 @@ const Card = (props) => {
 			</h2>
 			<hr className="border-black my-2" />
 			<p>
-				This is some short description text. Write a few lines here to
-				describe each item. This can be as long or as short as needed.
+				{props.description ||
+					'This is some short description text. Write a few lines here to describe each item. This can be as long or as short as needed.'}
 			</p>
-			<button className="border rounded-md py-1 px-3 bg-gray-950 text-white">
+			<button className="border rounded-md py-1 px-3 mt-2 bg-gray-950 text-white">
 				Visit
 			</button>
 		</div>
--- app/page.js
+++ app/page.js
@@ -2,6 +2,21 @@ import Image from 'next/image';
 import Card from './components/card';
 
 export default function Home() {
+	const draftingDescription = () => {
+		return 'From bathroom remodels to full luxury estates & offices, we are able to assist home & business owners, builders, brokers, interior designers and other drafting firms nationwide.';
+	};
+	const realEstateDescription = () => {
+		return 'Your Real Estate Team for Sioux Falls and the surrounding area. Helping you buy your next home and sell your current residence. Listings from Sioux Falls, Brandon, and more.';
+	};
+	const lotStoreDescription = () => {
+		return 'See where open lots are in the Sioux Falls area. Streamlining the process of finding open lots and providing buyers with the necessary information to make informed decisions.';
+	};
+	const coreCareDescription = () => {
+		return '';
+	};
+	const remmersionDescription = () => {
+		return 'Real Estate Media Management, creating engaging panoramic virtual tours and renderings, website development, and social media marketing. Let us help with your online digital presence.';
+	};
 	return (
 		<div className="grid grid-rows-[0px_1fr_0px] items-start justify-items-center p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start md:w-1/2 mx-auto">
@@ -14,11 +29,31 @@ export default function Home() {
 					priority
 				/>
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
-					<Card title="CoreDrafting" link="coredrafting.com" />
-					<Card title="CoreRealEstate" link="corerealestate.agency" />
-					<Card title="CoreLotStore" link="corelotstore.com" />
-					<Card title="CoreCare" link="corecare.services" />
-					<Card title="Remmersion" link="remmersion.com" />
+					<Card
+						title="CoreDrafting"
+						link="coredrafting.com"
+						description={draftingDescription()}
+					/>
+					<Card
+						title="CoreRealEstate"
+						link="corerealestate.agency"
+						description={realEstateDescription()}
+					/>
+					<Card
+						title="CoreLotStore"
+						link="corelotstore.com"
+						description={lotStoreDescription()}
+					/>
+					<Card
+						title="CoreCare"
+						link="corecare.services"
+						description={coreCareDescription()}
+					/>
+					<Card
+						title="Remmersion"
+						link="remmersion.com"
+						description={remmersionDescription()}
+					/>
 				</div>
 			</main>
 		</div>
